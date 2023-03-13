023f87bf8b451758598e524cd2edccccc84f47c1
mathew180 <mathewadavuruku@gmail.com>
2023-03-13 05:15:58 +0000

    pinterest

--- file_upload/starter/server.js
+++ file_upload/starter/server.js
@@ -1,32 +1,38 @@
-require('dotnev').config();
+// require('dotnev').config();
 require('express-async-errors')
-
+require('./db/connect');
+require("dotenv").config();
 const express = require("express");
 const app = express();
 
 const connectDB = require('./db/connect');
 
-const notFoundMiddleware = require('./middleware/not-found');
-const errorHandlerMiddleware = require('./middleware/error-Handler')
+//const notFoundMiddleware = require('./middleware/not-found');
+//const errorHandlerMiddleware = require('./middleware/error-Handler')
 
 app.get('/', (req, res) => {
     res.send('<h1>File upload starter</h1>')
 })
-
+//fv9MiAq60QWcFq34
 //middleware
-app.use(notFoundMiddleware);
-app.use(errorHandlerMiddleware)
+//app.use(notFoundMiddleware);
+//app.use(errorHandlerMiddleware)
 
-const port = process.env.port || 3000;
+const PORT = process.env.port || 3000;
 
-const start = async () => {
-    try{
-        await connectDB(prcess.env.Mongo_URI)
-          app.listen(port, console.log(`Server is listening on port ${port}...`))
-    }catch(error)
-    {
-        console.log(error);
-    }
-}
+app.listen(PORT, console.log(`Server is listening on port ${PORT}...`))
 
-start();
+// const start = async () => {
+//     try{
+//         await connectDB(process.env.Mongo_URI)
+//           app.listen(port, console.log(`Server is listening on port ${port}...`))
+//     }catch(error)
+//     {
+//         console.log(error);
+//     }
+// }
+
+// start();
+//mongodb+srv://mathewadavuruku:<fv9MiAq60QWcFq34>@cluster0.tkgzawi.mongodb.net/file-upload?retryWrites=true&w=majority
+
+//mongodb+srv://mathewadavuruku:<fv9MiAq60QWcFq34>@cluster0.tkgzawi.mongodb.net/?retryWrites=true&w=majority
